49a7be6cf0fc23d9cd7cc1954cff4268178a317a
AndersCan <anders.can1@gmail.com>
2020-03-11 11:59:10 +0100

    Types (#2)
    * refactor types
    
    * Update LICENSE
    
    * update lock file
    
    * patch directive function for browser
    
    * release 2.0.0-beta.2
    
    * release 2.0.0
    
    Co-authored-by: Alexander Pope <alex@pope-industries.com>

--- browser/directives/custom-element-render.js
+++ browser/directives/custom-element-render.js
@@ -1,5 +1,11 @@
 import { directive } from '@popeindustries/lit-html';
 
-export const customElementRender = directive(() => (part) => {
-  part.setValue(undefined);
+export const customElementRender = directive(() => {
+  const fn = (part) => {
+    part.setValue(undefined);
+  };
+
+  // Allow lit-html-server to identify this directive registered with lit-html
+  fn.isDirective = true;
+  return fn;
 });
